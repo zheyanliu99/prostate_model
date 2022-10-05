9d572504d43f60d80b48c8e213473d22b072a682
“Zheyan <zheyan.liu@columbia.edu>
2022-10-05 04:43:38 -0400

    add estimation and PE

--- analysis.py
+++ analysis.py
@@ -8,7 +8,9 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
 from statsmodels.regression.linear_model import OLS
 from statsmodels.tools import add_constant
 
@@ -21,13 +23,14 @@ X_test = df_test.drop(['id', 'train', 'lpsa'], axis=1)
 y_test = df_test.lpsa
 
 # %% Best-Set cv
+# plot
 results = []
 for k in range(1, 9):
     for combo in itertools.combinations(X.columns, k):
         X_train_bs = X[list(combo)]
         mse = -cross_val_score(LinearRegression(), X_train_bs, y, 
                                 cv=5, scoring='neg_mean_squared_error')
-        aresult = {'k':k, 'combo':combo, 'mse':np.mean(mse), 'mse_path':mse}
+        aresult = {'k':k, 'combo': list(combo), 'mse':np.mean(mse), 'mse_path':mse}
         results.append(aresult)
 
 results_df = pd.DataFrame(results)
@@ -59,13 +62,27 @@ plt.legend()
 _ = plt.title(
     f"Mean square error on each fold: coordinate descent"
 )
+
+# %% Best-Set cv
+reg = LinearRegression().fit(X[best_set['combo']], y)
+# estimation
+cdf = pd.DataFrame(reg.coef_, X[best_set['combo']].columns, columns=['Coefficients'])
+print(cdf)
+# %% Best-Set cv
+# prediction error 
+y_pred = reg.predict(X_test[best_set['combo']])
+mean_squared_error(y_test, y_pred)
+# %% Best-Set cv
+# std
+np.std(y_pred)/np.sqrt(len(y_pred))
+
 # %% Best-Set BIC
 results = []
 for k in range(1, 9):
     for combo in itertools.combinations(X.columns, k):
         X_train_bs = X[list(combo)]
         regr = OLS(y, add_constant(X_train_bs)).fit()
-        aresult = {'k':k, 'combo':combo, 'BIC':regr.bic}
+        aresult = {'k':k, 'combo':list(combo), 'BIC':regr.bic}
         results.append(aresult)
 
 results_df = pd.DataFrame(results)
@@ -87,6 +104,18 @@ ax.legend()
 _ = ax.set_title(
     f"Information-criterion for model selection"
 )
+# %% Best-Set cv
+reg = LinearRegression().fit(X[best_set['combo']], y)
+# estimation
+cdf = pd.DataFrame(reg.coef_, X[best_set['combo']].columns, columns=['Coefficients'])
+print(cdf)
+# %% Best-Set cv
+# prediction error 
+y_pred = reg.predict(X_test[best_set['combo']])
+mean_squared_error(y_test, y_pred)
+# %% Best-Set cv
+# std
+np.std(y_pred)/np.sqrt(len(y_pred))
 # %% Lasso 
 
 ## BIC
@@ -128,6 +157,19 @@ _ = ax.set_title(
     f"Information-criterion for model selection"
 )
 
+# %% Lasso bic
+reg = Lasso(alpha=alpha_bic).fit(X, y)
+# estimation
+cdf = pd.DataFrame(reg.coef_, X.columns, columns=['Coefficients'])
+print(cdf)
+# %% Lasso bic
+# prediction error 
+y_pred = reg.predict(X_test)
+mean_squared_error(y_test, y_pred)
+# %% Lasso bic
+# std
+np.std(y_pred)/np.sqrt(len(y_pred))
+
 
 # %% Lasso cv
 ## cross validation
@@ -157,10 +199,22 @@ _ = plt.title(
     f"Mean square error on each fold"
 )
 
-
+# %% Lasso cv
+reg = Lasso(alpha=lasso.alpha_).fit(X, y)
+# estimation
+cdf = pd.DataFrame(reg.coef_, X.columns, columns=['Coefficients'])
+print(cdf)
+# %% Lasso cv
+# prediction error 
+y_pred = reg.predict(X_test)
+mean_squared_error(y_test, y_pred)
+# %% Lasso cv
+# std
+np.std(y_pred)/np.sqrt(len(y_pred))
 
 
 # %% PCM
+## PCM cross validation
 scores_mse = []
 for k in range(1, 9):
     model = PCA(n_components=k)
@@ -169,10 +223,8 @@ for k in range(1, 9):
     mse = -cross_val_score(LinearRegression(), X_train_pca, y, 
                            cv=5, scoring='neg_mean_squared_error')
     scores_mse.append(np.mean(mse))
-min(scores_mse)
  
 index = np.argmin(scores_mse)
-index
  
 plt.plot(range(1, 9), scores_mse)
 plt.axvline(index + 1, color='k', linestyle='--', linewidth=1)
@@ -181,4 +233,19 @@ plt.ylabel('Mean Squared Error')
 plt.title('5-fold Cross-validation Error')
 plt.tight_layout()
 
-# %%
+# %% PCM cv
+pca = PCA(n_components=index+1)
+pca.fit(X)
+X_pca = pca.transform(X)
+X_test_pca = pca.transform(X_test)
+reg = Lasso(alpha=lasso.alpha_).fit(X_pca, y)
+# estimation
+cdf = pd.DataFrame(reg.coef_, ['PC' + str(i+1) for i in range(index+1)], columns=['Coefficients'])
+print(cdf)
+# %% PCM cv
+# prediction error 
+y_pred = reg.predict(X_test_pca)
+mean_squared_error(y_test, y_pred)
+# %% PCM cv
+# std
+np.std(y_pred)/np.sqrt(len(y_pred))
